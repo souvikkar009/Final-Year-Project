39aa732d33a61cf1d017f2dc452be7b08d8b4d09
Souvik Kar <souvikkar009@gmail.com>
2025-06-14 21:43:38 +0530

    timestamps are added in institute and organization model

--- acad-vault/backend/models/institute.model.js
+++ acad-vault/backend/models/institute.model.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 
-const institute_schema = mongoose.Schema({
-    // role
-    role: { type: String, default: "institute" },
-    _id: Number,
-    institute_name: String,
-    institute_id: Number,
-    password: String,
-    // insitute type based on the level of education
-    institute_type: {
-        type: String,
-        enum: [
-            "School",
-            "Board",
-            "College",
-            "University",
-            "Multi-Disciplinary Institute",
-        ],
-        default: null,
+const institute_schema = mongoose.Schema(
+    {
+        // role
+        role: { type: String, default: "institute" },
+        _id: Number,
+        institute_name: String,
+        institute_id: Number,
+        password: String,
+        // insitute type based on the level of education
+        institute_type: {
+            type: String,
+            enum: [
+                "School",
+                "Board",
+                "College",
+                "University",
+                "Multi-Disciplinary Institute",
+            ],
+            default: null,
+        },
+        institute_level: {
+            type: String,
+            enum: ["secondary", "higher_secondary", "higher_studies"], // currently there are three types of institute in our platform
+            default: null,
+        },
+        // For higher level institutions
+        program_levels: { type: [String], default: null }, // program level or degree level offered by the institute e.g. doctoral, undergraduation, postgraduation
+        program_names: { type: [String], default: null }, // degree e.g. Btech, BE, BCA, BBA etc offered by
     },
-    institute_level: {
-        type: String,
-        enum: ["secondary", "higher_secondary", "higher_studies"], // currently there are three types of institute in our platform
-        default: null,
-    },
-    // For higher level institutions
-    program_levels: { type: [String], default: null }, // program level or degree level offered by the institute e.g. doctoral, undergraduation, postgraduation
-    program_names: { type: [String], default: null }, // degree e.g. Btech, BE, BCA, BBA etc offered by
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Institute", institute_schema);
--- acad-vault/backend/models/organization.model.js
+++ acad-vault/backend/models/organization.model.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const organization_schema = mongoose.Schema({
-    // role
-    role: { type: String, default: "organization" },
+const organization_schema = mongoose.Schema(
+    {
+        // role
+        role: { type: String, default: "organization" },
 
-    _id: String,
-    orgid: String,
-    organization_name: String,
-    email: String,
-    password: String,
-    access_keys: {
-        type: mongoose.Schema.Types.Map,
-        default: {},
-    }, // every time user need data of students, he/she need to create an access key which will be stored in db
-    secret_key: String, // when a user registers, he/she will be assigned a secret key
-});
+        _id: String,
+        orgid: String,
+        organization_name: String,
+        email: String,
+        password: String,
+        access_keys: {
+            type: mongoose.Schema.Types.Map,
+            default: {},
+        }, // every time user need data of students, he/she need to create an access key which will be stored in db
+        secret_key: String, // when a user registers, he/she will be assigned a secret key
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Organization", organization_schema);
